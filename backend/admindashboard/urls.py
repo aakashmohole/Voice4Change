939ddeadd6d88349f4ad768d764f613c7933bc7d
Aakash <aakashmohole@gmail.com>
2025-03-28 20:22:24 +0530

    admin dashboard

--- backend/admindashboard/urls.py
+++ backend/admindashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AdminDashboardView, AssignFeedbackView, ExportFeedbackView
 
 urlpatterns = [
-    path("admin-dashboard/", AdminDashboardView.as_view(), name="admin-dashboard"),
+    path("dashboard/", AdminDashboardView.as_view(), name="admin-dashboard"),
     path("assign-feedback/<int:feedback_id>/", AssignFeedbackView.as_view(), name="assign-feedback"),
     path("feedback-export/", ExportFeedbackView.as_view(), name="feedback-export"),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
     
     path('api/vote/', include('votes.urls')),
     
-    path("api/dashboard/", include("dashboard.urls")),     
+    path("api/dashboard/", include("admindashboard.urls")),     
     
      
     # endpoint documentation
