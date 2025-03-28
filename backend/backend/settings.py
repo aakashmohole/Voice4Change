c0c8136c353976e00c118c0df3c3847061b32fad
Aakash <aakashmohole@gmail.com>
2025-03-28 15:02:45 +0530

    cores setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",  # Add this at the top,
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -207,8 +208,6 @@ SIMPLE_JWT = {
 }
 
 
-
-# If you want to allow all origins (Not recommended for production)
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
